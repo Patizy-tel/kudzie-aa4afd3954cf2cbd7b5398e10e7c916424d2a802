eb8dc46fb3554ef9f20f5579c9ec2caf72b16816
Tello <pateltanaka22@live.com>
2021-08-24 09:24:06 +0200

    d

--- src/app/controllers/admin/admin.module.ts
+++ src/app/controllers/admin/admin.module.ts
@@ -2,13 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ClarityModule} from '@clr/angular';
 import {AdminModule} from '../../views/admin-ui/admin.module'
+import {AgmCoreModule} from '@agm/core';
 import {AdminRoute} from './admin-route.routing'
 import {DashboardComponent} from './dashboard/dashboard.component'
 import {UsersComponent} from './Users/Users.component'
 import {InsuaranceComponent} from './insuarance/insuarance.component'
 @NgModule({
     imports: [
-        CommonModule, AdminModule, ClarityModule, AdminRoute
+        CommonModule, AdminModule, ClarityModule, AdminRoute, AgmCoreModule.forRoot({apiKey: 'AIzaSyD1MWtAHULMcX32FhTwXegpkwxU9OjBoUQ', libraries: ['places']})
     ],
     declarations: [
         DashboardComponent,
