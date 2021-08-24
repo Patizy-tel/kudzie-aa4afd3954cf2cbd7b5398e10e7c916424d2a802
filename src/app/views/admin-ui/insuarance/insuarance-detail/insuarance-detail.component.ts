0539adea12595810f1c75cf9801acb0ce32d4b98
Tello <pateltanaka22@live.com>
2021-08-24 09:33:58 +0200

    g

--- src/app/views/admin-ui/insuarance/insuarance-detail/insuarance-detail.component.ts
+++ src/app/views/admin-ui/insuarance/insuarance-detail/insuarance-detail.component.ts
@@ -21,7 +21,7 @@ import {
 } from "ng-apexcharts";
 import io from 'socket.io-client';
 
-const socket = io('https://backendpatientkudzie.herokuapp.com');
+const socket = io('http://localhost:5000');
 export type ChartOptions = {
   series: ApexAxisChartSeries;
   chart: ApexChart;
